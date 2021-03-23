b8f08179e191f76d4ec67424a47f6cb624dd3132
denisuryadi26 <deni.w4f@gmail.com>
2021-03-23 23:28:37 +0700

    Change Project Name

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
+import MateriFlexBox from './MateriFlexBox';
+import PositionReactNative from './PositionReactNative';
 import SampleComponen from './SampleComponent';
 import StylingReactNativeComponent from './StylingReactNativeComponent';
 
@@ -8,8 +10,10 @@ const App = () => {
   return (
     <View>
       <ScrollView>
-        <SampleComponen />
+        {/* <SampleComponen />
         <StylingReactNativeComponent />
+        <MateriFlexBox /> */}
+        <PositionReactNative />
       </ScrollView>
     </View>
   );
